d5098ed26f778e8a9a0ba1215cbd4f3b7d3b770b
james-work-account <31282758+james-work-account@users.noreply.github.com>
2024-09-18 11:32:00 +0100

    [ETFE-4527] point to emcs-tfe for trader known facts

--- app/featureswitch/core/config/FeatureSwitchingModule.scala
+++ app/featureswitch/core/config/FeatureSwitchingModule.scala
@@ -25,7 +25,7 @@ import javax.inject.Singleton
 @Singleton
 class FeatureSwitchingModule extends Module with FeatureSwitchRegistry {
 
-  val switches: Seq[FeatureSwitch] = Seq(UserAllowList, StubGetTraderKnownFacts, EnableNRS)
+  val switches: Seq[FeatureSwitch] = Seq(UserAllowList, EnableNRS)
 
   override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] = {
     Seq(
@@ -37,11 +37,6 @@ class FeatureSwitchingModule extends Module with FeatureSwitchRegistry {
 case object UserAllowList extends FeatureSwitch {
   override val configName: String = "features.allowListEnabled"
   override val displayName: String = "Enable the User Allow List"
-}
-
-case object StubGetTraderKnownFacts extends FeatureSwitch {
-  override val configName: String = "features.stub-get-trader-known-facts"
-  override val displayName: String = "Use stub to get trader known facts"
 }
 
 case object EnableNRS extends FeatureSwitch {
