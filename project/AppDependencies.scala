44ed52d4867ac3c350496e9273bee314dddbd6c4
nickg <148371638+nickg-hmrc@users.noreply.github.com>
2025-04-01 16:22:29 +0100

    Use HttpClientV2 in UserAllowListConnector and bump versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,8 +6,8 @@ object AppDependencies {
 
   val playSuffix = "-play-30"
   val scalatestVersion = "3.2.19"
-  val hmrcBootstrapVersion = "9.5.0"
-  val hmrcMongoVersion = "2.3.0"
+  val hmrcBootstrapVersion = "9.11.0"
+  val hmrcMongoVersion = "2.6.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
