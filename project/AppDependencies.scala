ac34acc272ecd2214e667ae8adb45b2277ed17c4
nickg <148371638+nickg-hmrc@users.noreply.github.com>
2025-04-15 15:59:17 +0100

    Make updates suggested by platops-pr-bot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,7 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"             %% s"play-frontend-hmrc$playSuffix"     % "11.2.0",
+    "uk.gov.hmrc"             %% s"play-frontend-hmrc$playSuffix"     % "11.13.0",
     "uk.gov.hmrc"             %% s"bootstrap-frontend$playSuffix"     %  hmrcBootstrapVersion,
     "uk.gov.hmrc.mongo"       %% s"hmrc-mongo$playSuffix"             %  hmrcMongoVersion,
     "com.google.inject"       %   "guice"                             % "5.1.0"
